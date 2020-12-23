403d76328173e1f8ea436b5ef8d8f05d0906ac5d
Dhaval Shah <dhaval.shah@oracle.com>
2020-12-23 17:54:15 -0500

    update login

--- py/app.py
+++ py/app.py
@@ -187,13 +187,13 @@ class Client:
         self.info(all_windows)
         try:
             current_window = set([self.br.current_window_handle])
-            self.info(list(current_window)[0].title)
+            self.info(current_window)
             other_window = (all_windows - current_window).pop()
-            self.info(other_window.title)
-            self.br.switch_to.window(other_window)
+            self.info(other_window)
+            self.info(self.br.switch_to.window(other_window).getTitle())
         except:
             current_window = self.br.window_handles[0]
-            self.br.switch_to.window(current_window)
+            self.info(self.br.switch_to.window(current_window).getTitle())
             
         self.info(current_window)
 
