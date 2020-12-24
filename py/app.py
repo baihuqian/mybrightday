4d732667392299cea87b7628da238ddf4a62987b
Dhaval Shah <dhaval.shah@oracle.com>
2020-12-24 09:36:58 -0500

    update login

--- py/app.py
+++ py/app.py
@@ -101,8 +101,9 @@ class Client:
         options = Options()
         #google="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36"
         #google="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
+        google='Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:46.0) Gecko/20100101 Firefox/46.0'
 
-        #options.add_argument(f'user-agent={google}')
+        options.add_argument(f'user-agent={google}')
         options.add_argument("no-default-browser-check")
         options.add_argument("no-first-run")
         options.add_argument("no-sandbox")
